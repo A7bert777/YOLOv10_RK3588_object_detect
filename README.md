1302baeb44b30a60d3cb2369ca9466f95e44a0d7
Zhang Tianqi <zhangtianqi@yijiahe.com>
2025-03-26 21:01:02 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# YOLOv10_RK3588_object_detect
+**YOLO11_RK3588_object_detect 目标检测仓库**
+
+1.项目代码介绍
+
+src/main.cpp ：主程序运行文件
+
+src/postprocess.cpp: 模型推理后的后处理代码
+
+src/yolo11.cpp：模型初始化、推理、反初始化等函数代码
+
+include/postprocess.h、yolo11.h：各函数声明
+
+2.配置文件介绍
+
+yolov11/3rdparty 中是第三方库
+
+yolo11/build 是编译位置
+
+yolo11/inputimage 是输入图片所在文件夹
+
+yolo11/outputimage 是输出图片所在文件夹
+
+yolo11/model 是RKNN模型以及标签名txt文件所在文件夹
+
+yolo11/rknn_lib 是瑞芯微官方动态库librknnrt.so所在位置
+
+3.编译运行
+
+在编译前需在源文件中全局搜索 “*************” ，为路径修改标记
+
+**①cd build**
+
+**②cmake ..**
+
+**③make**
+
+**④./rknn_yolo11_demo**
+
+
+
+
+
+CSDN地址：[【YOLO11部署至RK3588】模型训练→转换RKNN→开发板部署_yolov11 rk3588-CSDN博客](https://blog.csdn.net/A_l_b_ert/article/details/143814080?spm=1001.2014.3001.5501)
+
+QQ咨询（not free）：2506245294
---
+++ build/CMakeFiles/rknn_yolov10_demo.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/firefly/yolov10_github/src/main.cc" "/home/firefly/yolov10_github/build/CMakeFiles/rknn_yolov10_demo.dir/src/main.cc.o"
+  "/home/firefly/yolov10_github/src/postprocess.cc" "/home/firefly/yolov10_github/build/CMakeFiles/rknn_yolov10_demo.dir/src/postprocess.cc.o"
+  "/home/firefly/yolov10_github/src/yolov10.cc" "/home/firefly/yolov10_github/build/CMakeFiles/rknn_yolov10_demo.dir/src/yolov10.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../"
+  "../utils"
+  "../3rdparty/stb_image"
+  "../3rdparty/jpeg_turbo/include"
+  "../3rdparty/librga/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/firefly/yolov10_github/build/utils.out/CMakeFiles/imageutils.dir/DependInfo.cmake"
+  "/home/firefly/yolov10_github/build/utils.out/CMakeFiles/fileutils.dir/DependInfo.cmake"
+  "/home/firefly/yolov10_github/build/utils.out/CMakeFiles/imagedrawing.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
