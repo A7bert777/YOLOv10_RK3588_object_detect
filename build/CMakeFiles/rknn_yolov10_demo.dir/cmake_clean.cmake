b8801a174530d174a3a592720b5ba76d2bf68800
A7bert777 <2506245294@qq.com>
2025-03-27 17:27:37 +0800

    first commit

---
+++ build/CMakeFiles/rknn_yolov10_demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rknn_yolov10_demo.dir/src/main.cc.o"
+  "CMakeFiles/rknn_yolov10_demo.dir/src/postprocess.cc.o"
+  "CMakeFiles/rknn_yolov10_demo.dir/src/yolov10.cc.o"
+  "rknn_yolov10_demo"
+  "rknn_yolov10_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rknn_yolov10_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
